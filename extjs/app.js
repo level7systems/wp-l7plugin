c567e9e3aa057bc044d03ea404b9be2a0409522f
level7 <level7@uk-www1.l7dev.co.cc>
2014-08-27 21:53:22 +0000

    ExtJS 5 app skeleton generated

---
+++ extjs/app.js
@@ -0,0 +1,18 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'Level7',
+
+    extend: 'Level7.Application',
+    
+    autoCreateViewport: 'Level7.view.main.Main'
+	
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to Level7.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
---
+++ extjs/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Level7.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Level7'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
