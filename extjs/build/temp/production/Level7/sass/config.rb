2bc9391f30958958f287f070f405279110034a3c
Kamil Adryjanek <kamil@level7systems.co.uk>
2014-09-02 11:35:56 +0000

    Removed all build staff

--- extjs/build/temp/production/Level7/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
-require '../../../../../sass/config.rb'
-cache_path = '/enc/var/www/wp_l7plugin/extjs/build/.sass-cache'
--- extjs/build/temp/production/Level7/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /enc/var/www/wp_l7plugin/extjs/app.js
