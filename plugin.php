d7e8ac704a553087b3ae5278bb476df12f3304b4
Kamil Adryjanek <kamil@level7systems.co.uk>
2014-09-18 10:36:51 +0000

    Working on plugin configuration

--- plugin.php
+++ plugin.php
@@ -15,4 +15,31 @@ define('LEVEL7_PATH', dirname(__FILE__));
 
 //require LEVEL7_PATH . '/inc/core.php';
 //$level7 = new Level7Plugin();
-?>
+
+function level7_init_scripts() {
+
+    if (is_admin()) {
+        // only backend
+        
+    } else {
+        /// non-admin section
+        wp_enqueue_script('level7-login', plugins_url('/js/level7-login.js', __FILE__), array('jquery'));
+    }
+}
+
+
+function level7_menu() {
+    
+    add_menu_page("Level7", "Level7", 'manage_options', 'level7-menu', 'level7_options');
+}
+
+function level7_options() {
+    echo '<div class="wrap">';
+    echo '<p><strong>TO-DO: Add some config options...</p>';
+    echo '</div>';
+}
+
+add_action('wp_enqueue_scripts', 'level7_init_scripts');
+
+// admin menu
+add_action('admin_menu', 'level7_menu');
