9f44634d37c01ef39cc2d3941b8b703ced3b43d0
Kamil Adryjanek <kamil@level7systems.co.uk>
2015-04-15 13:52:46 +0000

    Templates updated

--- includes/frontend/L7P_Inline.php
+++ includes/frontend/L7P_Inline.php
@@ -8,27 +8,28 @@
  * file that was distributed with this source code.
  */
 
-// TODO:
-function url_for()
+// displays charge for a SMS message
+function l7p_inline_sms_charge()
 {
-    
+    return 'l7p_currency_symbol(l7p_inline_charge(\'sms\'), 0, true)';
 }
 
-// inlines
-
-function l7p_inline_user_charge($atts)
+// displays monthly charge for a User account
+function l7p_inline_user_charge()
 {
-    return l7p_inline_charge('user');
+    return 'l7p_currency_symbol(l7p_inline_charge(\'user\'), 0, true)';
 }
 
-function l7p_inline_user_unlimited($atts)
+// displays monthly charge for a Unlimited plan
+function l7p_inline_user_unlimited()
 {
-    return l7p_inline_charge('user-S');
+    return 'l7p_currency_symbol(l7p_inline_charge(\'user-S\'), 0, true)';
 }
 
-function l7p_inline_user_unlimited_int($atts)
+// displays monthly charge for a Unlimited International plan
+function l7p_inline_user_unlimited_int()
 {
-    return l7p_inline_charge('user-A');
+    return 'l7p_currency_symbol(l7p_inline_charge(\'user-A\'), 0, true)';
 }
 
 function l7p_inline_charge($service)
@@ -43,139 +44,323 @@ function l7p_inline_charge($service)
     return l7p_currency_symbol($charge, $currency);
 }
 
-// Displays termination route first name (if different from previous one).
+// Termination
+
+// displays fixed line termination cost for the current user
+function l7p_inline_term_local_fixed()
+{
+    return '(isset($domestic) && isset($domestic[\'fixed\'])) ? (($domestic[\'fixed\'] * 100) <= 100) ? l7p_currency_symbol($domestic[\'fixed\'],1,true) : l7p_currency_symbol($domestic[\'fixed\']) : \'<!-- TERM_LOCAL_FIXED not defined -->\'';
+}
+
+// displays fixed line termination cost for the current user
+function l7p_inline_term_local_mobile()
+{
+    return '(isset($domestic) && isset($domestic[\'mobile\'])) ? (($domestic[\'mobile\'] * 100) <= 100) ? l7p_currency_symbol($domestic[\'mobile\'],1,true) : l7p_currency_symbol($domestic[\'mobile\']) : \'<!-- TERM_LOCAL_MOBILE not defined -->\'';
+}
+
+// displays termination name rate
+function l7p_inline_term_name()
+{
+    return '(isset($term_name)) ? __($term_name) : \'<!-- TERM_NAME not defined -->\'';
+}
+
+// displays per minute termination rate
+function l7p_inline_term_prefixes()
+{
+    return '(isset($term_data) && isset($term_data[\'patterns\'])) ? implode(", ",$term_data[\'patterns\']) : \'<!-- TERM_PREFIXES not defined -->\'';
+}
+
+// displays per minute termination rate
+function l7p_inline_term_min_price()
+{
+    return 'isset($term_data) && isset($term_data[\'rate\']) ? (($term_data[\'rate\'] * 100) <= 100) ? l7p_currency_symbol($term_data[\'rate\'],1,true) : l7p_currency_symbol($term_data[\'rate\']) : \'<!-- TERM_MIN_PRICE not defined -->\'';
+}
+
+// displays minimim per minute termination charge
+function l7p_inline_term_min_charge()
+{
+    return 'isset($min_charge) ? l7p_currency_symbol($min_charge,1,true) : \'<!-- TERM_MIN_CHARGE not defined -->\'';
+}
+
+// displays termination route first name (if different from previous one)
 function l7p_inline_term_letter()
 {
     return 'isset($firstletter) ? $firstletter : \'<!-- TERM_LETTER not defined -->\'';
 }
 
-// Displays mobile termination rate
+// displays mobile termination rate
 function l7p_inline_term_mobile()
 {
     return '(($term_data[\'mobile\'] * 100) <= 100) ? l7p_currency_symbol($term_data[\'mobile\'],1,true) : l7p_currency_symbol($term_data[\'mobile\'])';
 }
 
-// Displays fixed termination rate
+// displays fixed termination rate
 function l7p_inline_term_fixed()
 {
     return '(($term_data[\'fixed\'] * 100) <= 100) ? l7p_currency_symbol($term_data[\'fixed\'],1,true) : l7p_currency_symbol($term_data[\'fixed\'])';
 }
 
-function l7p_inline_term_countries()
+// display country name
+function l7p_inline_term_route_country()
 {
-    // TODO
+    return 'isset($country_name) ? $country_name : l7p_get_country_name_from_query()';
+}
+
+// termination route name
+function l7p_inline_term_route_name()
+{
+    return '(isset($term_name)) ? __($term_name) : \'<!-- TERM_ROUTE_NAME not defined -->\'';
+}
+
+// displays termination route per minut rate
+function l7p_inline_term_route_rate()
+{
+    return '(($term_data[\'rate\'] * 100) <= 100) ? l7p_currency_symbol($term_data[\'rate\'],1,true) : l7p_currency_symbol($term_data[\'rate\'])';
+}
+
+// displays termination route  connection fee
+function l7p_inline_term_conn_fee()
+{
+    return '(($term_data[\'connection\'] * 100) <= 100) ? l7p_currency_symbol($term_data[\'connection\'],1,true) : l7p_currency_symbol($term_data[\'connection\'])';
+}
+
+// displays termination route prefixes
+function l7p_inline_term_route_prefixes()
+{
+    return 'implode(", ",$term_data[\'patterns\'])';
 }
 
 // country url
 function l7p_inline_term_route_url()
 {
-    return '\'?country=\'.$country_name';
-    //return 'url_for(__(\'@country_rates\').\'?country=\'.$country_name)';
+    return 'l7p_url_for(\'@country_rates\', array(\'country\' => $country_name))';
 }
 
-// display country name
-function l7p_inline_term_route_country()
+# DDIs
+
+// displays DDI country two letter code
+function l7p_inline_ddi_country_code()
 {
-    return 'isset($country_name) ? $country_name : \'<!-- TERM_ROUTE_COUNTRY not defined -->\'';
+    return 'strtolower($ddi_data[\'country_code\'])';
 }
 
-// TODO
-function l7p_inline_app_url()
+// displays DDI country name
+function l7p_inline_ddi_country_name()
 {
-    // TODO
-    return "app_url(get_entry_app()).__('/app')";
+    return '$ddi_data[\'country_name\']';
 }
+
+// displays DDI country telephone code
+function l7p_inline_ddi_country_tel_code()
+{
+    return '$ddi_data[\'country_tel_code\']';
+}
+
+// displays DDI area code
+function l7p_inline_ddi_area_code()
+{
+    return '$ddi_data[\'area_code\']';
+}
+
+// displays DDI city name
+function l7p_inline_ddi_city_name()
+{
+    return '$ddi_data[\'city\']';
+}
+
+// displays DDI country URL
+function l7p_inline_ddi_country_url()
+{
+    return 'l7p_url_for(\'@numbers\', array(\'country\' => $ddi_data[\'country_name\']))';
+}
+
+// displays DDI setup fee
+function l7p_inline_ddi_setup_fee()
+{
+    return 'l7p_currency_symbol($ddi_data[\'NRC\'])';
+}
+
+// displays DDI monthly subscription fee
+function l7p_inline_ddi_monthly_fee()
+{
+    return 'l7p_currency_symbol($ddi_data[\'MRC\'])';
+}
+
+// displays DDI per minute usage fee
+function l7p_inline_ddi_min_rate()
+{
+    return 'l7p_currency_symbol($ddi_data[\'min_rate\'])';
+}
+
+// displays buy DDI url
+function l7p_inline_ddi_buy_url()
+{
+    return '(isset($state)) ? l7p_url_for(\'@numbers_state\', array(\'state\' => $state)) : l7p_url_for(\'@numbers\', array(\'country\' => $ddi_data[\'country_name\']))';
+}
+
+// displays buy DDI in selected U.S. state url
+function l7p_inline_ddi_state_url()
+{
+    return '(isset($state_data) && isset($state_data[\'state_name\'])) ? l7p_url_for(\'@numbers_state\', array(\'state\' => ucwords(strtolower($state_data[\'state_name\'])))) : \' \'';
+}
+
+// displays buy DDI in selected U.S. state url
+function l7p_inline_ddi_state_name()
+{
+    return '(isset($state_data) && isset($state_data[\'state_name\'])) ? $state_data[\'state_name\'] : \' \'';
+}
+
 # Phones
-// desk category page URL
+
+// Phone - desk category page URL
 function l7p_inline_phone_desk_url()
 {
-    return url_for(__('@phones_group') . '?group=' . __('Desk Phones'));
+    return 'l7p_url_for(\'@phones_group\', array(\'group\' => \'Desk Phones\'))';
 }
 
-// desk min. price
+// Phone - desk min. price
 function l7p_inline_phone_desk_min_price()
 {
-    return isset($min_price) ? currency_symbol($min_price['Desk Phones']) : '<!-- PHONE_DESK_MIN_PRICE not defined -->';
+    return '(isset($min_price)) ? l7p_currency_symbol($min_price[\'Desk Phones\']) : \'<!-- PHONE_DESK_MIN_PRICE not defined -->\'';
 }
 
-// DECT category page URL
+// Phone - DECT category page URL
 function l7p_inline_phone_dect_url()
 {
-    return url_for(__('@phones_group') . '?group=' . __('DECT Phones'));
+    return 'l7p_url_for(\'@phones_group\', array(\'group\' => \'DECT Phones\'))';
 }
 
-// DECT min. price
+// Phone - DECT min. price
 function l7p_inline_phone_dect_min_price()
 {
-    return isset($min_price) ? currency_symbol($min_price['DECT Phones']) : '<!-- PHONE_DECT_MIN_PRICE not defined -->';
+    return 'isset($min_price) ? l7p_currency_symbol($min_price[\'DECT Phones\']) : \'<!-- PHONE_DECT_MIN_PRICE not defined -->\'';
 }
 
-// conference category page URL
+// Phone - conference category page URL
 function l7p_inline_phone_conf_url()
 {
-
-    return url_for(__('@phones_group') . '?group=' . __('Conference Phones'));
+    return 'l7p_url_for(\'@phones_group\', array(\'group\' => \'Conference Phones\'))';
 }
-/*
-    PHONE_CONF_MIN_PRICE:
-    function: "isset($min_price) ? currency_symbol($min_price['Conference Phones']) : '<!-- PHONE_CONF_MIN_PRICE not defined -->'"
-    description: Phone - conference min. price
-    PHONE_ADAPTOR_URL:
-    function: "url_for(__('@phones_group').'?group='.__('VoIP Adaptors'))"
-    description: Phone - adaptors category page URL
-    PHONE_ADAPTOR_MIN_PRICE:
-    function: "(isset($min_price)) ? currency_symbol($min_price['VoIP Adaptors']) : '<!-- PHONE_ADAPTOR_MIN_PRICE not defined -->'"
-    description: Phone - adaptors min. price
-    PHONE_ACCESSORY_URL:
-    function: "url_for(__('@phones_group').'?group='.__('Accessories'))"
-    description: Phone - accessories category page URL
-    PHONE_ACCESSORY_MIN_PRICE:
-    function: "currency_symbol($min_price['Accessories'])"
-    description: Phone - accessories min. price
-    PHONE_GROUP_NAME:
-    function: "$sf_request->getParameter('group')"
-    description: Phone - current group name
-    PHONE_URL:
-    function: "url_for(__('@phone_page').'?group='.$sf_request->getParameter('group').'&model='.$phone_data['name'])"
-    description: Phone - URL to the current phone
-    PHONE_NAME:
-    function: "$phone_data['name']"
-    description: Phone - name
-    PHONE_MANUFACTURER:
-    function: "$phone_data['manufacturer']"
-    description: Phone - manufacturer
-    PHONE_MODEL:
-    function: "$phone_data['model']"
-    description: Phone - model
-    PHONE_SHORT_DESCRIPTION:
-    function: "$phone_data['short_description']"
-    description: Phone - short description
-    PHONE_STOCK:
-    function: "$phone_data['stock']"
-    description: Phone - stock
-    PHONE_PRICE:
-    function: "currency_symbol($phone_data['price'])"
-    description: Phone - price
-    PHONE_THUMB_IMG:
-    function: "$phone_data['thumb_img']"
-    description: Phone - thumbnail image
-    PHONE_IMG:
-    function: "$phone_data['img']"
-    description: Phone - image
-    PHONE_GROUP_URL:
-    function: "url_for(__('@phones_group').'?group='.$sf_request->getParameter('group'))"
-    description: Phone - group URL
-    PHONE_REVIEWS_COUNT:
-    function: "$phone_data['review_count']"
-    description: Phone - reviews count
-    PHONE_RATING:
-    function: "get_rating($phone_data['rating'])"
-    description: Phone - ratings stars
-    PHONE_REVIEWS_URL:
-    function: "url_for(__('@phone_reviews').'?group='.$sf_request->getParameter('group').'&model='.$phone_data['name'])"
-    description: Phone - reviews URL
-    PHONE_DESCRIPTION:
-    function: "str_replace('.pdf">','.pdf" target="_blank">',convert_markdown_text($phone_data['description']))"
-    description: Phone - description
-*/
 
+// Phone - conference min. price
+function l7p_inline_phone_conf_min_price()
+{
+    return 'isset($min_price) ? l7p_currency_symbol($min_price[\'Conference Phones\']) : \'<!-- PHONE_CONF_MIN_PRICE not defined -->\'';
+}
+
+// Phone - adaptors category page URL
+function l7p_inline_phone_adaptor_url()
+{
+    return 'l7p_url_for(\'@phones_group\', array(\'group\' => \'VoIP Adaptors\'))';
+}
+
+// Phone - adaptors min. price
+function l7p_inline_phone_adaptor_min_price()
+{
+    return 'isset($min_price) ? l7p_currency_symbol($min_price[\'VoIP Adaptors\']) : \'<!-- PHONE_ADAPTOR_MIN_PRICE not defined -->\'';
+}
+
+// Phone - accessories category page URL
+function l7p_inline_phone_accessory_url()
+{
+    return 'l7p_url_for(\'@phones_group\', array(\'group\' => \'Accessories\'))';
+}
+
+// Phone - accessories min. price
+function l7p_inline_phone_accessory_min_price()
+{
+    return 'l7p_currency_symbol($min_price[\'Accessories\'])';
+}
+
+// Phone - current group name
+function l7p_inline_phone_group_name()
+{
+    // TODO
+    return 'l7p_get_group_name_from_query()';
+}
+
+// Phone - URL to the current phone
+function l7p_inline_phone_url()
+{
+    return 'l7p_url_for(\'@phone_page\', array(\'group\' => l7p_get_group_name_from_query(), \'model\' => $phone_data[\'name\']))';
+}
+
+// Phone - name
+function l7p_inline_phone_name()
+{
+    return '"$phone_data[\'name\']';
+}
+
+// Phone - manufacturer
+function l7p_inline_phone_manufacturer()
+{
+    return '$phone_data[\'manufacturer\']';
+}
+
+//  Phone - model
+function l7p_inline_phone_model()
+{
+    return '$phone_data[\'model\']';
+}
+
+// Phone - short description
+function l7p_inline_phone_short_description()
+{
+    return '$phone_data[\'short_description\']';
+}
+
+// Phone - stock
+function l7p_inline_phone_stock()
+{
+    return '$phone_data[\'stock\']';
+}
+
+// Phone - price
+function l7p_inline_phone_price()
+{
+    return 'l7p_currency_symbol($phone_data[\'price\'])';
+}
+
+// Phone - thumbnail image
+function l7p_inline_phone_thumb_img()
+{
+    return '$phone_data[\'thumb_img\']';
+}
+
+// Phone - image
+function l7p_inline_phone_img()
+{
+    return '$phone_data[\'img\']';
+}
+
+// Phone - group URL
+function l7p_inline_phone_group_url()
+{
+    return 'l7p_url_for(\'@phones_group\', array(\'group\' => l7p_get_group_name_from_query()))';
+}
+
+// Phone - reviews count
+function l7p_inline_phone_reviews_count()
+{
+    return '$phone_data[\'review_count\']';
+}
+
+// Phone - ratings stars
+function l7p_inline_phone_rating()
+{
+    // TODO
+    return 'get_rating($phone_data[\'rating\'])';
+}
+
+// Phone - reviews URL
+function l7p_inline_phone_reviews_url()
+{
+    // TODO: verify
+    return 'l7p_url_for(\'@phone_reviews\', array(\'group\' => l7p_get_group_name_from_query(), \'model\' => $phone_data[\'name\']))';
+}
+
+// Phone - description
+function l7p_inline_phone_description()
+{
+    return 'str_replace(\'.pdf">\',\'.pdf" target="_blank">\',convert_markdown_text($phone_data[\'description\']))';
+}
