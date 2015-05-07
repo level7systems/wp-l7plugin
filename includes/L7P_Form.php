7009500e9ab448b53d711fe34c7bab3625d74431
Kamil Adryjanek <kamil@level7systems.co.uk>
2015-05-07 13:24:38 +0000

    Form class refactoring + new methods added

--- includes/L7P_Form.php
+++ includes/L7P_Form.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Level 7 Systems Ltd. platform.
  *
@@ -9,34 +8,99 @@
  * file that was distributed with this source code.
  */
 
-function text_input($args)
+class L7P_Form
 {
-    $name = isset($args['name']) ? esc_attr($args['name']) : 'undefined';
-    $section = isset($args['section']) ? esc_attr($args['section']) : false;
-    $value = isset($args['value']) ? esc_attr($args['value']) : '';
-    $placeholder = isset($args['placeholder']) ? esc_attr($args['placeholder']) : '';
-    $pre = isset($args['pre']) ? esc_attr($args['pre']) : false;
-    $post = isset($args['post']) ? esc_attr($args['post']) : false;
-    $help = isset($args['help']) ? esc_attr($args['help']) : false;
-    
-    // field section
-    if ($section) {
-        $name = sprintf("%s[%s]", $section, $name);
+    public static function label($args)
+    {
+        $id = isset($args['id']) ? esc_attr($args['id']) : 'undefined';
+        $label = isset($args['label']) ? esc_attr($args['label']) : 'undefined';
+        
+        echo "<label for='$id'>$label</label>";
     }
     
-    // pre field 
-    if ($pre) {
-        echo $pre;
+    public static function input($args)
+    {
+        $name = isset($args['name']) ? esc_attr($args['name']) : 'undefined';
+        $id = isset($args['id']) ? esc_attr($args['id']) : $name;
+        $type = isset($args['type'])? esc_attr($args['type']) : 'text';
+        $section = isset($args['section']) ? esc_attr($args['section']) : false;
+        $value = isset($args['value']) ? esc_attr($args['value']) : '';
+        $placeholder = isset($args['placeholder']) ? esc_attr($args['placeholder']) : '';
+        $pre = isset($args['pre']) ? esc_attr($args['pre']) : false;
+        $post = isset($args['post']) ? esc_attr($args['post']) : false;
+        $help = isset($args['help']) ? esc_attr($args['help']) : false;
+        $style = isset($args['style']) ? "style='".$args['style']."'" : '';
+
+        // field section
+        if ($section) {
+            $name = sprintf("%s[%s]", $section, $name);
+        }
+
+        // pre field 
+        if ($pre) {
+            echo $pre;
+        }
+
+        echo "<input type='text' id='$id' name='$name' value='$value' placeholder='$placeholder' $style />";
+
+        // post field
+        if ($post) {
+            echo $post;
+        }
+
+        if ($help) {
+            echo "<p><small>$help</small></p>";
+        }
+    }
+
+
+    public static function text_input($args)
+    {
+        $args['type'] = 'text';
+        return self::input($args);
     }
     
-    echo "<input type='text' name='$name' value='$value' placeholder='$placeholder' />";
-    
-    // post field
-    if ($post) {
-        echo $post;
+    public static function password_input($args)
+    {
+        $args['type'] = 'password';
+        self::input($args);
     }
     
-    if ($help) {
-        echo "<p><small>$help</small></p>";
+    public static function select($args)
+    {
+        $name = isset($args['name']) ? esc_attr($args['name']) : 'undefined';
+        $id = isset($args['id']) ? esc_attr($args['id']) : $name;
+        $section = isset($args['section']) ? esc_attr($args['section']) : false;
+        $choices = isset($args['choices']) ? $args['choices'] : array();
+        $value = isset($args['value']) ? esc_attr($args['value']) : '';
+        $pre = isset($args['pre']) ? esc_attr($args['pre']) : false;
+        $post = isset($args['post']) ? esc_attr($args['post']) : false;
+        $help = isset($args['help']) ? esc_attr($args['help']) : false;
+        $style = isset($args['style']) ? "style='".$args['style']."'" : '';
+
+        // field section
+        if ($section) {
+            $name = sprintf("%s[%s]", $section, $name);
+        }
+
+        // pre field 
+        if ($pre) {
+            echo $pre;
+        }
+
+        echo "<select id='$id' name='$name' $style />";
+        foreach ($choices as $id => $label) {
+            echo "<option value='$id'>$label</option>";
+        }
+        echo "</select>";
+        
+        // post field
+        if ($post) {
+            echo $post;
+        }
+
+        if ($help) {
+            echo "<p><small>$help</small></p>";
+        }
     }
 }
